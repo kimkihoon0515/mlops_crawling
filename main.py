890bbeb6658380e48a05b2cef235852100d200ea
kimkihoon0515 <tega1996@naver.com>
2022-12-10 12:51:22 +0900

    repo name 수정

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from github_utils import get_github_repo, upload_github_issue
 
 if __name__ == "__main__":
     access_token = os.environ['MY_GITHUB_TOKEN']
-    repository_name = "github-action-with-python"
+    repository_name = "mlops_crawling"
 
     seoul_timezone = timezone('Asia/Seoul')
     today = datetime.now(seoul_timezone)
